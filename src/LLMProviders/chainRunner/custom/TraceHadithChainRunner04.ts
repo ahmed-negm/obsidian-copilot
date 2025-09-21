f75d31b93e0d35c722f01c86a00cfe630974e6bf
Ahmed Negm <ahmed.negm@mendix.com>
2025-09-21 23:02:52 +0200

    Read bio from Shamela

--- src/LLMProviders/chainRunner/custom/TraceHadithChainRunner04.ts
+++ src/LLMProviders/chainRunner/custom/TraceHadithChainRunner04.ts
@@ -1,6 +1,7 @@
 import ChainManager from "@/LLMProviders/chainManager";
 import { BaseSimpleChainRunner, SystemMessage } from "./BaseSimpleChainRunner";
 import { TraceHadithChainRunner03Input } from "./TraceHadithChainRunner03";
+import { getShamelaContent } from "./shamelaHelper";
 
 export interface TraceHadithChainRunner04Input extends TraceHadithChainRunner03Input {
   allNarratorIndex: number;
@@ -15,8 +16,16 @@ export class TraceHadithChainRunner04 extends BaseSimpleChainRunner {
   }
 
   async formatInput(messages: SystemMessage[]) {
+    const shamelaContent = await getShamelaContent(
+      this.input.allNarrators[this.input.allNarratorIndex].shamelaIndex,
+      this.input.allNarrators[this.input.allNarratorIndex + 1].shamelaIndex
+    );
     return [
-      { role: "user", content: `What do you think about number ${this.input.allNarratorIndex}?` },
+      messages[0],
+      {
+        role: "user",
+        content: `Please summerize the following bio for ${this.input.allNarrators[this.input.allNarratorIndex].name} from تهذيب الكمال :\n\n${shamelaContent}`,
+      },
     ];
   }
 
