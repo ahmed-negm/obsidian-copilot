215e3597667f1ecc6a927739431058010743035f
Ahmed Negm <ahmed.negm@mendix.com>
2025-09-27 16:53:10 +0200

    Implement range in extract narrators workflow

--- src/LLMProviders/chainRunner/custom/utils/formatUtils.ts
+++ src/LLMProviders/chainRunner/custom/utils/formatUtils.ts
@@ -7,3 +7,13 @@ export function toArabicDigits(str: string | number): string {
   const arabic = "٠١٢٣٤٥٦٧٨٩";
   return String(str).replace(/[0-9]/g, (d) => arabic[parseInt(d)]);
 }
+
+/**
+ * Convert Arabic digits in a string to English digits.
+ * @param str String or number to convert
+ * @returns String with English digits
+ */
+export function toEnglishDigits(str: string | number): string {
+  const arabicDigits = "٠١٢٣٤٥٦٧٨٩";
+  return String(str).replace(/[٠-٩]/g, (d) => arabicDigits.indexOf(d).toString());
+}
--- src/LLMProviders/chainRunner/custom/workflows/ExtractNarratorsWorkflowRunner.ts
+++ src/LLMProviders/chainRunner/custom/workflows/ExtractNarratorsWorkflowRunner.ts
@@ -2,6 +2,7 @@ import { WorkflowRunner } from "./../base/WorkflowRunner";
 import ChainManager from "@/LLMProviders/chainManager";
 import { ExtractNarratorsState } from "../models/State";
 import { ExtractNarratorsStep } from "../steps/ExtractNarratorsStep";
+import { toArabicDigits, toEnglishDigits } from "../utils";
 
 export class ExtractNarratorsWorkflowRunner extends WorkflowRunner<ExtractNarratorsState> {
   constructor(chainManager: ChainManager, args: string) {
@@ -9,6 +10,20 @@ export class ExtractNarratorsWorkflowRunner extends WorkflowRunner<ExtractNarrat
   }
 
   protected registerSteps() {
-    return [new ExtractNarratorsStep(this.state)];
+    if (this.state.args) {
+      const range = this.state.args.split("-");
+      if (range.length === 2) {
+        const start = parseInt(toEnglishDigits(range[0]));
+        const end = parseInt(toEnglishDigits(range[1]));
+        if (!isNaN(start) && !isNaN(end) && start > 0 && end >= start) {
+          const steps: ExtractNarratorsStep[] = [];
+          for (let i = start; i <= end; i++) {
+            steps.push(new ExtractNarratorsStep({ ...this.state, args: toArabicDigits(i) }));
+          }
+          return steps;
+        }
+      }
+    }
+    return [new ExtractNarratorsStep({ ...this.state, args: toArabicDigits(this.state.args) })];
   }
 }
